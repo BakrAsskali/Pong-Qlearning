c16debb09aab42343860b0b0962fc448fa721ced
BakrAsskali <bakrasskali@gmail.com>
2024-01-16 14:30:03 +0100

    QLearning Agent working

--- agent.py
+++ agent.py
@@ -1,50 +1,70 @@
 import numpy as np
-import os
-import pickle
-import collections
 import random
 
-class Qlearning:
-    """
-     class for Q-learning .
-    Parameters
-    ----------
-    alpha : float
-        learning rate
-    gamma : float
-        temporal discounting rate
-    eps : float
-        probability of random action vs. greedy action
-    eps_decay : float
-        epsilon decay rate. Larger value = more decay
-    """
 
-    def __init__(self, alpha, gamma, eps, eps_decay=0.):
-        # Agent parameters
+def getReward(ball, bar):
+    if bar.top <= ball.centery <= bar.bottom:
+        return 1
+    else:
+        return -1
+
+
+def get_state_from_pos(y, bar_height, window_height):
+    a = 0
+    b = bar_height
+    for i in range(0, window_height, bar_height):
+        if a <= y <= b:
+            return int((b / bar_height) - 1)
+        else:
+            a += bar_height
+            b += bar_height
+
+
+class Qlearning:
+
+    def __init__(self, alpha, gamma, eps, window_height, bar_height, eps_decay=0.):
+        self.states = int(window_height / bar_height)
         self.alpha = alpha
         self.gamma = gamma
         self.eps = eps
         self.eps_decay = eps_decay
-
+        # Q-Table
+        self.qTable = np.matrix(np.zeros(shape=[self.states, self.states]))
+        self.rewards = []
 
     def get_action(self, s):
-       pass
+        if s < 0:
+            s = 0
+        if s >= self.states:
+            s = self.states - 1
+        a = np.argmax(self.qTable[s, :])
 
+        if a < 0:
+            a = 0
+        if a > self.states:
+            a = self.states
+        return a
 
-    def update(self, s, s_, a, a_, r):
-        """
-        Perform the Q-Learning update of Q values.
-        Parameters
-        ----------
-        s : string
-            previous state
-        s_ : string
-            new state
-        a : (i,j) tuple
-            previous action
-        a_ : (i,j) tuple
-            new action. NOT used by Q-learner!
-        r : int
-            reward received after executing action "a" in state "s"
-        """
-        pass
+    def update(self, s, bar, ball):
+        global s_
+        if s < 0:
+            s = 0
+        if s >= self.states:
+            s = self.states - 1
+
+        exp_threshold = random.uniform(0, 1)
+        # if exploiting
+        if exp_threshold > self.eps:
+            a = self.get_action(s)
+        else:  # if exploring
+            a = random.choice([i for i in range(self.states)])
+
+        if a >= 0:
+            s_ = a
+
+        reward = getReward(ball, bar)
+        self.rewards.append(reward)
+
+        self.qTable[s, a] += self.alpha * (reward + self.gamma * np.max(self.qTable[s_, :]) - self.qTable[s, a])
+
+        return s_ * bar.height
--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 import game as g
-import agent as ag
 import matplotlib.pylab as plt
 import numpy as np
+import tkinter as tk
+
+
+class UserInterface:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.var = tk.IntVar()
+        self.selected_game_type = None
+
+    def select_mode(self):
+        self.selected_game_type = self.var.get()
+        self.root.destroy()
+
+    def run(self):
+        tk.Label(self.root, text="Choose game mode:").pack()
+        tk.Radiobutton(self.root, text="Agent RL vs Human", variable=self.var, value=1).pack(anchor='w')
+        tk.Radiobutton(self.root, text="Agent RL vs Agent AI", variable=self.var, value=2).pack(anchor='w')
+        tk.Radiobutton(self.root, text="Agent RL vs Agent RL", variable=self.var, value=3).pack(anchor='w')
+        tk.Button(self.root, text="Submit", command=self.select_mode).pack()
+
+        self.root.mainloop()
+
 
 def plot_agent_reward(rewards):
     """ Function to plot agent's accumulated reward vs. iteration """
@@ -11,21 +32,25 @@ def plot_agent_reward(rewards):
     plt.xlabel('Episode')
     plt.show()
 
-class GameLearning:
-    def __init__(self, alpha=0.5, gamma=0.9, epsilon=0.01):
 
-        self.agent = ag.Qlearning(alpha , gamma , epsilon)
-        self.game = g.Game(self.agent)
+class GameLearning:
+    def __init__(self, game_type, alpha=0.5, gamma=0.9, epsilon=0.01):
+        self.alpha = alpha
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.game = g.Game(game_type)
         self.games_played = 0
 
-    def beginPlaying(self, episodes):
+    def begin_playing(self, episodes):
+        self.game.play(episodes)
 
-            self.game.play()
-
-
-
-#
 
 if __name__ == '__main__':
-    gl = GameLearning()
-    gl.beginPlaying(200)
+    ui = UserInterface()
+    ui.run()
+
+    if ui.selected_game_type is not None:
+        gl = GameLearning(ui.selected_game_type)
+        gl.begin_playing(1000)
+    else:
+        print("No game type selected.")
